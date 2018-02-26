9fc99ecf9013ef1288ad758a20e81f9c970ac83f
kbpsystem <koleen.bp@outlook.com>
2018-02-27 05:58:17 +0800

    Added Smooth Scroll feature

--- script.js
+++ script.js
@@ -120,3 +120,24 @@ function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+
+//Back To Up Pop-Up Function
+
+window.onscroll = function() {
+    scrollFunction()    
+};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
